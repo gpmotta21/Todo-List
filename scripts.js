8a80544b45964700224c510e8e9c5ebdd289e5a0
Gabriel Pacheco Motta <gpmotta21@gmail.com>
2021-11-20 14:47:23 -0300

    Launched todo site

--- scripts.js
+++ scripts.js
@@ -1,37 +1,89 @@
-var todosAmount = document.getElementsByClassName('todos')
-var userInput = document.querySelector('#userInput')
+var todoList = document.querySelector('#todoList');
+var input = document.querySelector('#userInput');
+var todosAmount = document.getElementsByClassName('todos');
+var error = document.querySelector('#error');
 
-const dataBase = []
+const setItem = (db) => localStorage.setItem('todoList', JSON.stringify(db));
+const getItem = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
 
-const createItem = (text) =>{
-
-    if(text == ""){
-        document.querySelector('#error').style.opacity = '1'
-    }
-    else if(text !== "" && todosAmount.length < 5){
-        
-        document.querySelector('#error').style.opacity = '0'
-        
-        const newElement = document.createElement('div')
-        newElement.classList.add('todos')
-        newElement.innerHTML = `
-        <button type="button">v</button>
+function createTodo(text, status, index) {
+    var newElement = document.createElement('label');
+    newElement.classList.add('todos');
+    newElement.innerHTML = `
+        <input type="checkbox" class='checkBox' data-index=${index} ${status}>
         <div>${text}</div>
-        <button type="button" id="delete" onclick="del()">X</button>
-        `
-        document.querySelector('#todoList').appendChild(newElement)
-}
+        <button type="button" data-index=${index} onclick="removeTodo(this)">X</button>
+    `
+    todoList.appendChild(newElement);
+
+    requestAnimationFrame(() =>
+        setTimeout(() => {
+            newElement.style.opacity = '1';
+        }))
 }
 
-function clearScreen(){
-    var todoList = document.querySelector('#todoList')
-    while(todoList.firstChild){
-        todoList.removeChild(todoList.lastChild)
+// Send item userInput and checkbox status to de DB
+function sendToDb() {
+    const dB = getItem()
+
+    if(todosAmount.length == 5){
+        error.textContent = 'You can only create up to 5 todo´s';
+        error.style.opacity = '1';
+    }
+    else if(input.value == ''){
+        error.style.opacity = '1';
+        error.innerHTML = 'Please create a todo';
+    }
+    else if(input.value !== '' && todosAmount.length < 5){
+        error.style.opacity = '0';
+        dB.push({ 'text': input.value, 'status': '' });
+        setItem(dB);
+    }
+    showOnScreen()
+
+}
+
+// Send item.text and item.status to createElement() in order to create HTML elements
+function showOnScreen() {
+    clearScreen()
+
+    const dB = getItem()
+    dB.forEach((item, index) => createTodo(item.text, item.status, index));
+}
+
+function clearScreen() {
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
     }
 }
 
-function sendToDb(){
-    clearScreen()
-    dataBase.push(userInput.value)
-    dataBase.forEach(item => createItem(item))
+// Will locate the button index and remove it from the DB
+function removeTodo(d) {
+    const dB = getItem()
+
+    var index = d.getAttribute('data-index');
+    dB.splice(index, 1);
+    setItem(dB)
+
+    showOnScreen()
 }
+
+// Locate specific checkbox that was checked
+function clickItem(event) {
+    if (event.target.type === 'checkbox') {
+        var checkBoxIndex = event.target.dataset.index;
+        saveCheckStatus(checkBoxIndex)
+    }
+}
+
+// Change checkbox status
+function saveCheckStatus(index) {
+    const dB = getItem()
+
+    dB[index].status = dB[index].status === '' ? 'checked' : '';
+    setItem(dB)
+
+}
+
+todoList.addEventListener('click', clickItem);
+showOnScreen()
